96bee3019ef2cb950a2afa4dcab780779a4f5127
lyz112358 <3214832117@qq.com>
2025-05-26 23:26:20 +0800

    Add files via upload

---
+++ pages/index/data.js
@@ -0,0 +1,51 @@
+export const list0 = [{
+    name: '车辆申请',
+    value: 0,
+}, {
+    name: '我的车辆',
+    value: 1,
+}]
+
+
+export const list1 = [{
+    name: '标签1',
+    value: 0,
+}, {
+    name: '标签2',
+    value: 1,
+}, {
+    name: '标签3',
+    value: 2,
+}, {
+    name: '标签4',
+    value: 3,
+}, {
+    name: '标签5',
+    value: 4,
+},
+    {
+        name: '标签6',
+        value: 5,
+    },{
+        name: '标签7',
+        value: 6,
+    },{
+        name: '标签8',
+        value: 7,
+    },{
+        name: '标签9',
+        value: 8,
+    },{
+        name: '标签10',
+        value: 9,
+    }]
+
+export const list2 = [
+     {
+    name: '标签2',
+    value: 1,
+}, {
+    name: '标签3',
+    value: 2,
+}
+]
